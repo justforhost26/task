848c999eeda66701e23a60aa2cd1926d8f04e8df
PARAG MODI <77090196+justforhost26@users.noreply.github.com>
2021-06-28 21:04:56 +0530

    Update settings.py

--- craftin_images_task/settings.py
+++ craftin_images_task/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1gaz1ublug!o^)c5(n09=qvwhav@2)j+bnc-7&=#unv!f!v0h3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
