3ac4ec2ca401f3149d5541c2aae53e511c65e940
PARAG MODI <77090196+justforhost26@users.noreply.github.com>
2021-06-28 20:52:02 +0530

    Update settings.py

--- craftin_images_task/settings.py
+++ craftin_images_task/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-1gaz1ublug!o^)c5(n09=qvwhav@2)j+bnc-7&=#unv!f!v0h3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
